2319522a69505bc488d5935c2a569aede46af87c
csdc23bb-thomas-gutjahr <71706791+csdc23bb-thomas-gutjahr@users.noreply.github.com>
2022-01-20 18:38:10 +0100

    Added About Us

--- frontend/src/components/pages/About.js
+++ frontend/src/components/pages/About.js
@@ -2,11 +2,16 @@ import React from 'react'
 
  const About = () => {
     return (
-        <div>
-            <h1>About this app</h1>
-            <p className="my-1">Test about page (checking if nav works) </p>
-
-            
+        <div className="container-aboutus">
+            <div class="item-aboutus">
+                <p>thomas lb https://bit.ly/3zp4vdx</p>
+            </div>
+            <div class="item-aboutus">
+                <p a >dominik d https://bit.ly/3gxuw1k</p>
+            </div>
+            <div class="item-aboutus">
+                <p>thomas g https://bit.ly/2Tjh9Kp</p>
+            </div>
         </div>
     )
 }
